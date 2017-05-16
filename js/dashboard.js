dd159dd508a7c004b449c1771b3e94aae5fd0eff
Jorge Sebastian Santamaria larrotta <Jorge Sebastian Santamaria larrotta>
2017-05-16 00:10:02 -0500

    Adding buttons to change cell Scale

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,7 +4,7 @@ function Dashboard(){
       startDate: new DayPilot.Date("2017-04-01").firstDayOfMonth(),	     
       days: 800,
       timeHeaders: [
-          { groupBy: "Month", format: "MMMM yyyy" },
+          { groupBy: "Month", format: "MMMM/yy" },
           { groupBy: "Day", format: "d" }
         ],
       scale: "Day",
@@ -12,11 +12,12 @@ function Dashboard(){
       treeEnabled: true,
       events: [],
       dynamicEventRendering: "Disabled"
-    }		
+  }
+
 }
 
 Dashboard.prototype.init= function(){	
-	this.getInitialData();
+	this.getInitialData();  
 }
 
 Dashboard.prototype.getInitialData = function(){
@@ -36,23 +37,60 @@ Dashboard.prototype.getEvents = function(){
 	});
 }
 
-Dashboard.prototype.createCheduler= function(){
-	
+Dashboard.prototype.createCheduler = function(){
+	console.log(this.config);
 	var config = this.config;
 	var dp = $("#dp").daypilotScheduler(config);
 	
-	dp.onEventClick = function(args) {
-		console.log(args);
-        var modal = new DayPilot.Modal();
-        modal.onClosed = function(args) {
-            // reload all events
-            var result = args.result;
-            if (result && result.status === "OK") {
-                loadEvents();
-            }
-        };
-        modal.showHtml("<h1>HOLA WORLD</h1>");
-    };
+	// dp.onEventClick = function(args) {
+	// 	console.log(args);
+ //        var modal = new DayPilot.Modal();
+ //        modal.onClosed = function(args) {
+ //            // reload all events
+ //            var result = args.result;
+ //            if (result && result.status === "OK") {
+ //                loadEvents();
+ //            }
+ //        };
+ //        modal.showHtml("<h1>HOLA WORLD</h1>");
+ //    };
+}
+
+Dashboard.prototype.changeCellScale = function(obj){
+  var _this = obj;
+  var type = $(this).data('type');
+  if(type == "day"){
+
+    _this.config.timeHeaders = [
+      { groupBy: "Month", format: "MMMM/yy" },
+      { groupBy: "Day", format: "d" }
+    ];
+
+    _this.config.scale = "Day";
+
+  }else{
+
+    _this.config.timeHeaders = [
+      { groupBy: "Month", format: "MMMM/yy" }
+    ];
+
+    _this.config.scale = "Month";
+  }
+
+  _this.updateScheduler(); 
+
+}
+
+Dashboard.prototype.updateScheduler = function(){
+  this.cleanScheduler(); 
+  this.createCheduler();
+}
+
+Dashboard.prototype.cleanScheduler = function(){
+  $("#dp").removeAttr("style");
+  $("#dp").removeAttr("class");
+  $("#dp").html("");
+  console.log("limpio");
 }
 
  
